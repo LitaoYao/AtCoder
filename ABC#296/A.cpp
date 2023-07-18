8bc768b3acf108c5cb5180d38280cf1dbf3a44ea
LitaoYao <yaobob@foxmail.com>
2023-07-18 13:29:29 +0800

    supply solving the simple problems A&B

---
+++ ABC#296/A.cpp
@@ -0,0 +1,39 @@
+﻿// https://atcoder.jp/contests/abc296/tasks/abc296_a
+#include <iostream>
+using namespace std;
+void solve()
+{
+	int n;
+	cin >> n;
+	char x, y;
+	cin >> x;
+	bool valid = true;
+	for (int i = 1; i < n; ++i)
+	{
+		cin >> y;
+		if (x == y)
+		{
+			valid = false;
+		}
+		x = y;
+	}
+	cout << (valid ? "Yes" : "No") << endl;
+	return;
+}
+void fastRead()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+int main()
+{
+	fastRead();
+	int t;
+	t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
---
+++ ABC#296/B.cpp
@@ -0,0 +1,37 @@
+﻿// https://atcoder.jp/contests/abc296/tasks/abc296_b
+#include <iostream>
+using namespace std;
+void solve()
+{
+	int n = 8;
+	char x;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+		{
+			cin >> x;
+			if (x == '*')
+			{
+				cout << char('a' + j - 1) << n - i + 1 << endl;
+			}
+		}
+	}
+	return;
+}
+void fastRead()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+int main()
+{
+	fastRead();
+	int t;
+	t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
