94f82468afaaa38cf8a3c62c1a2a293283b95815
LitaoYao <yaobob@foxmail.com>
2023-08-09 16:25:16 +0800

    E is a mathematical task. Quite interesting.

---
+++ ABC#313/E.cpp
@@ -0,0 +1,52 @@
+﻿// https://atcoder.jp/contests/abc313/tasks/abc313_e
+#include <iostream>
+#include <string>
+using namespace std;
+const int MOD = 998244353;
+void solve()
+{
+	int n;
+	string s;
+	cin >> n;
+	cin >> s;
+	bool valid = true;
+	for (int i = 0; i < n - 1; ++i)
+	{
+		if ('1' != s[i] && '1' != s[i + 1])
+		{
+			valid = false;
+			break;
+		}
+	}
+	if (!valid)
+	{
+		cout << -1;
+	}
+	else
+	{
+		long long ans = 0;
+		for (int i = n - 1; i > 0; --i)
+		{
+			ans = (ans + (s[i] - '0' - 1) * (ans + 1) + 1) % MOD;
+		}
+		cout << ans << endl;
+	}
+	return;
+}
+void fastRead()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+int main()
+{
+	fastRead();
+	int t;
+	t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
