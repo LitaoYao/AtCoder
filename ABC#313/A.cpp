296aace11922337a303ffa3a593da0755811660e
LitaoYao <yaobob@foxmail.com>
2023-08-09 20:15:29 +0800

    nothing to say..

---
+++ ABC#313/A.cpp
@@ -0,0 +1,41 @@
+﻿// https://atcoder.jp/contests/abc313/tasks/abc313_a
+#include <iostream>
+using namespace std;
+void solve()
+{
+	int n;
+	cin >> n;
+	int score;
+	int ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> score;
+		if (0 == i)
+		{
+			ans = score * 1000;
+		}
+		else
+		{
+			ans = ans / 1000 * 1000 + max(ans % 1000, score - ans / 1000 + 1);
+		}
+	}
+	cout << ans % 1000 << endl;
+	return;
+}
+void fastRead()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+int main()
+{
+	fastRead();
+	int t;
+	t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
---
+++ ABC#313/B.cpp
@@ -0,0 +1,51 @@
+﻿// https://atcoder.jp/contests/abc313/tasks/abc313_b
+#include <iostream>
+#include <vector>
+using namespace std;
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<bool> loser(n + 1, false);
+	int x, y;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> x >> y;
+		loser[y] = true;
+	}
+	int winner = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (!loser[i])
+		{
+			if (winner)
+			{
+				winner = -1;
+				break;
+			}
+			else
+			{
+				winner = i;
+			}
+		}
+	}
+	cout << winner << endl;
+	return;
+}
+void fastRead()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+int main()
+{
+	fastRead();
+	int t;
+	t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
